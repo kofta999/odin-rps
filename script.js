28f769a78e6f8f7c03926a3eae16a87eda27d1c5
kofta999 <mostafaxxx555@gmail.com>
2023-11-12 20:48:55 +0200

    Create playRound function
    The purpose of this fucntion is to pick a winner in rock paper scissors
    from user input compared to the computer's response

--- script.js
+++ script.js
@@ -3,3 +3,53 @@ function getComputerChoice() {
   const index = Math.floor(Math.random() * 3);
   return items[index];
 }
+
+/**
+ *
+ * @param {string} playerSelection
+ * @param {string} computerSelection
+ */
+function playRound(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase();
+  computerSelection = computerSelection.toLowerCase();
+  let result;
+
+  if (playerSelection === "rock") {
+    switch (computerSelection) {
+      case "rock":
+        result = "Deuce!";
+        break;
+      case "paper":
+        result = "You lose! Paper beats Rock";
+        break;
+      case "scissors":
+        result = "You win! Rock beats Scissors";
+    }
+  } else if (playerSelection === "paper") {
+    switch (computerSelection) {
+      case "rock":
+        result = "You win! Paper beats rock";
+        break;
+      case "paper":
+        result = "Deuce!";
+        break;
+      case "scissors":
+        result = "You lose! Scissors beats paper";
+    }
+  } else if (playerSelection === "scissors") {
+    switch (computerSelection) {
+      case "rock":
+        result = "You lose! Rock beats Scissors";
+        break;
+      case "paper":
+        result = "You win! Scissors beats paper";
+        break;
+      case "scissors":
+        result = "Deuce!";
+    }
+  } else {
+    result = "Invalid Input";
+  }
+  console.log(result);
+  return result;
+}
